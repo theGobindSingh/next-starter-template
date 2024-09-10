0e1d51ac4ea0862bb5f0b8efd7e0ed41213da7d1
theGobindSingh <thesinghgobind@gmail.com>
2024-09-10 22:41:32 +0530

    fixed errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import type { AppProps } from 'next/app';
 import { Global } from '@emotion/react';
 import globalStyles from '@styles/global';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
