c00247468ba099f3100d8c123ebaa87e59f3d8ad
theGobindSingh <thesinghgobind@gmail.com>
2024-09-22 19:04:20 +0530

    Update eslint rules to enforce arrow function component definition

--- modules/home/index.tsx
+++ modules/home/index.tsx
@@ -1,6 +1,8 @@
 import { HomeWrapper } from '@modules/home/styles';
 import { HomeProps } from '@modules/home/types';
 
-export default function Home({ className }: HomeProps) {
-  return <HomeWrapper className={className}>Hello Home</HomeWrapper>;
-}
+const Home = ({ className }: HomeProps) => (
+  <HomeWrapper className={className}>Hello Home</HomeWrapper>
+);
+
+export default Home;
